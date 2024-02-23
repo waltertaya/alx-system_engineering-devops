2cbaceb5960af856aa82adf9fd77aebbc98e48db
waltertaya <waltermittybecker5@gmail.com>
2024-02-24 00:09:13 +0300

    Updated installing package Flask using pip3 v 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Install a package
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Install Flask package
 package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
