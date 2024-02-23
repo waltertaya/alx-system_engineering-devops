cb8feb32e733cbad83b326a7b9afa136dd0d7a24
waltertaya <waltermittybecker5@gmail.com>
2024-02-23 23:59:59 +0300

    Intro to puppet CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+# Install a package
 package { 'puppet-lint':
     ensure   => '2.5.0',
     provider => 'gem'
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# Execute a command
 exec { 'pkill':
   command => 'pkill -9 -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
