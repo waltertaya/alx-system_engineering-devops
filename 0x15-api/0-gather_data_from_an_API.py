9416ef6e5af2567d688225c6ce021da7fe60cf46
waltertaya <waltermittybecker5@gmail.com>
2024-03-25 17:23:41 +0300

    Updated: Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,13 +8,25 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id  = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/todos/{employee_id}".format(employee_id=employee_id)
-    url_users = "https://jsonplaceholder.typicode.com/users/{employee_id}".format(employee_id=employee_id)
+
+    url = "https://jsonplaceholder.typicode.com"
+    url_todos = "{url}/todos".format(url=url)
+    url_users = "{url}/users/{id}".format(url=url, id=sys.argv[1])
     response_user = requests.get(url_users)
-    response = requests.get(url)
-    response_json = response.json()
+    response_todos = requests.get(url_todos, params={'userId': sys.argv[1]})
+    response_todos_json = response_todos.json()
     response_user_json = response_user.json()
-    print(response_json)
-    print(response_user_json)
-    #{'userId': 4, 'id': 67, 'title': 'quia voluptatibus voluptatem quos similique maiores repellat', 'completed': False}
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    for todos in response_todos_json:
+        if todos.get('completed') is True:
+            NUMBER_OF_DONE_TASKS += 1
+            TOTAL_NUMBER_OF_TASKS += 1
+        else:
+            TOTAL_NUMBER_OF_TASKS += 1
+    print("Employee {} is done with tasks({}/{}):".format
+          (response_user_json.get('name'),
+           NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for todos in response_todos_json:
+        if todos.get('completed') is True:
+            print("\t {}".format(todos.get('title')))
